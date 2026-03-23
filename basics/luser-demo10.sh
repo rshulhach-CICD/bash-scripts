8439dac62d9f53693be76ae7356f477801c6b80e
rshulhach-CICD <r.shulhach@gmail.com>
2026-03-23 17:44:21 +0100

    Fix backup file path and cp command syntax

--- basics/luser-demo10.sh
+++ basics/luser-demo10.sh
@@ -18,11 +18,11 @@ backup_file() {
   # Make sure the file exists.
   if [[ -f "${FILE}" ]]
   then
-    local BACKUP_FILE="var/tmp/$(basename ${FILE}).$(date +%F-%N)"
+    local BACKUP_FILE="/var/tmp/$(basename "${FILE}").$(date +%F-%N)"
     log "Backing up ${FILE} to ${BACKUP_FILE}."
 
     # The exit status of the function will be the exit status of the cp command.
-    cp -p {FILE} ${BACKUP_FILE}
+    cp -p "${FILE}" "${BACKUP_FILE}"
   else
     # The file does not exist, so return a non-zero exit status.
     return 1
