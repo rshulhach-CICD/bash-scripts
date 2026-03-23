a85d61eec6ebf60f89c610a0d4161cd68d55f399
rshulhach-CICD <r.shulhach@gmail.com>
2026-03-23 17:51:41 +0100

    Rename variable USER to USER_NAME in luser-demo12.sh

--- admin/luser-demo12.sh
+++ admin/luser-demo12.sh
@@ -10,20 +10,25 @@ then
 fi
 
 # Assume the first argument is the user to delete.
-USER="${1}"
+USER_NAME="${1}"
+
+if [[ "${#}" -lt 1 ]]
+then
+  echo "Usage: ${0} USER_NAME" >&2
+  exit 1
+fi
 
 # Delete the user.
-userdel ${USER}
+userdel "${USER_NAME}"
 
 # Make sure the user got deleted.
 if [[ "${?}" -ne 0 ]]
 then
-  echo "The account ${USER} was NOT deleted." >&2
+  echo "The account ${USER_NAME} was NOT deleted." >&2
   exit 1
 fi
 
 # Tell the user the account was deleted.
-echo "The account ${USER} was deleted."
+echo "The account ${USER_NAME} was deleted."
 
 exit 0
-
