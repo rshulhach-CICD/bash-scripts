ec854e8683c333225af574e08d43b6faa5870e6b
rshulhach-CICD <r.shulhach@gmail.com>
2026-03-25 15:25:30 +0100

    Enhance luser-demo13.sh for clarity and functionality
    Updated comments for clarity and improved script functionality to handle IPv4 argument.

--- basics/luser-demo13.sh
+++ basics/luser-demo13.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # This script shows the open network ports on a system.
-# Use -4 as an argument to limit to tcpv4 ports.
+# Use -4 as an argument to limit output to IPv4 ports.
 
-PORTS=$(netstat -4nutl | grep ':' | awk '{print $4}' | awk -F ':' '{print $NF}')
+NETSTAT_OPTIONS='-nutl'
+
+if [[ "${1}" = '-4' ]]
+then
+  NETSTAT_OPTIONS='-4nutl'
+fi
+
+PORTS=$(netstat ${NETSTAT_OPTIONS} | grep ':' | awk '{print $4}' | awk -F ':' '{print $NF}' | sort -n | uniq)
 echo "${PORTS}"
-
